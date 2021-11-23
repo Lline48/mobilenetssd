95b874782b06e678dcff99de9bf18a4546861807
Lline48 <92071814+Lline48@users.noreply.github.com>
2021-11-23 10:26:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -152,9 +152,9 @@ def event_handle(event):
         if msg =="สวัสดี"
             replyObj = TextSendMessage(text="เออ...ดีด้วย")
         elif msg == "กินข้าวไหม" :
-            reply obj = textSendMessage(text="กินซิ หิวมาก")
+            replyObj = textSendMessage(text="กินซิ หิวมาก")
         elif msg =="ไปเที่ยวกันไหม" :
-            reply obj = textSendMessage(text="ไปดิ")
+            replyobj = textSendMessage(text="ไปดิ")
         else :
             replyObj = TextSendMessage(text="ไม่รู้ไม่ชี้")
         line_bot_api.reply_message(rtoken, replyObj)
